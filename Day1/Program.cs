3bd14d37541b2f3fed838faa3cc5f3eae1c21897
Leslie Richardson <lerich@microsoft.com>
2021-12-02 23:25:56 -0600

    Added Days 1 & 2

---
+++ Day1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace Day1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] list = File.ReadAllLines("C:/Users/lerich/OneDrive - Microsoft/source/advent-of-code-2021/Day1/input.txt");
+            List<int> depths = new List<int>();
+            for (int i = 0; i < list.Length; i++)
+            {
+                depths.Add(int.Parse(list[i]));
+            }
+
+            Console.WriteLine("Part 1: " + Part1(depths));
+            Console.WriteLine("Part 2: " + Part2(depths));
+        }
+
+        static int Part1(List<int> depths)
+        {
+            int increases = 0;
+            for (int i = 1; i < depths.Count; i++)
+            {
+                if (depths[i] > depths[i - 1]) increases++;
+            }
+            return increases;
+        }
+
+        static int Part2(List<int> depths)
+        {
+            int increases = 0;
+            for (int i = 2; i < depths.Count - 1; i++)
+            {
+                if ((depths[i + 1] + depths[i] + depths[i - 1]) > (depths[i] + depths[i - 1] + depths[i - 2])) increases++;
+            }
+            return increases;
+        }
+    }
+}
---
+++ Day2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace Day2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] list = File.ReadAllLines("C:/Users/lerich/OneDrive - Microsoft/source/advent-of-code-2021/Day2/input.txt");
+            List<string> commands = list.ToList();
+
+
+            Console.WriteLine("Part 1: " + Part1(commands));
+            Console.WriteLine("Part 2: " + Part2(commands));
+        }
+
+        static int Part1(List<string> commands)
+        {
+            int horizontal = 0;
+            int depth = 0;
+
+            foreach (string command in commands)
+            {
+                string[] split = command.Split(' ');
+                if (split[0].Equals("forward"))
+                {
+                    horizontal += int.Parse(split[1]);
+                }
+                else if (split[0].Equals("up"))
+                {
+                    depth -= int.Parse(split[1]);
+                }
+                else // split[0].Equals("down"))
+                {
+                    depth += int.Parse(split[1]);
+                }
+            }
+
+            return horizontal * depth;
+        }
+
+        static int Part2(List<string> commands)
+        {
+            int horizontal = 0;
+            int depth = 0;
+            int aim = 0;
+
+            foreach (string command in commands)
+            {
+                string[] split = command.Split(' ');
+                if (split[0].Equals("forward"))
+                {
+                    horizontal += int.Parse(split[1]);
+                    depth = depth + (int.Parse(split[1]) * aim);
+                }
+                else if (split[0].Equals("up"))
+                {
+                    aim -= int.Parse(split[1]);
+                }
+                else // split[0].Equals("down"))
+                {
+                    aim += int.Parse(split[1]);
+                }
+            }
+
+            return horizontal * depth;
+        }
+    }
+}
