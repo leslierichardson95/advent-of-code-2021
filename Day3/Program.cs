f99eb42716b64fec6361e43786dbbb5de71d659d
Leslie Richardson <lerich@microsoft.com>
2021-12-04 17:40:59 -0600

    Added Days 3 & 4

---
+++ Day3/Program.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace Day3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] list = File.ReadAllLines("C:/Users/lerich/OneDrive - Microsoft/source/advent-of-code-2021/Day3/input.txt");
+            List<string> binaryNumbers = list.ToList();
+
+
+            //Console.WriteLine("Part 1: " + Part1(binaryNumbers));
+            Console.WriteLine("Part 2: " + Part2(binaryNumbers));
+        }
+
+        static double Part1(List<string> binaryNumbers)
+        {
+            string gammaRate = "";
+            string epsilonRate = "";
+
+            List<int> ones = new List<int>();
+            for (int i = 0; i < binaryNumbers[0].Length; i++) ones.Add(0);
+
+            foreach (string binaryNumber in binaryNumbers)
+            {
+                for (int i = 0; i < binaryNumber.Length; i++)
+                {
+                    if (binaryNumber[i] == '1') ones[i]++;
+
+                }
+            }
+            for (int i = 0; i < ones.Count; i++)
+            {
+                if (ones[i] > (binaryNumbers.Count - ones[i]) )
+                {
+                    gammaRate += "1";
+                    epsilonRate += "0";
+                }
+                else
+                {
+                    gammaRate += "0";
+                    epsilonRate += "1";
+                }
+            }
+
+            return Convert.ToInt32(gammaRate, 2) * Convert.ToInt32(epsilonRate, 2);
+        }
+
+        static double Part2(List<string> binaryNumbers)
+        {
+            List<string> o2Numbers = new List<string>();
+            List<string> co2Numbers = new List<string>();
+            for (int i = 0; i < binaryNumbers.Count; i++)
+            {
+                o2Numbers.Add(binaryNumbers[i]);
+                co2Numbers.Add(binaryNumbers[i]);
+            }
+
+            int bitSize = binaryNumbers[0].Length;
+            for (int i = 0; i < bitSize; i++)
+            {
+
+                //// O2 Rating
+                List<int> o2Ones = CalculateOnes(o2Numbers);
+                if (o2Ones != null)
+                {
+                    if (o2Ones[i] >= (o2Numbers.Count - o2Ones[i])) // one is most common, zero is least common
+                    {
+                        int j = 0;
+                        while (j < o2Numbers.Count) // remove numbers not matching bit criteria (1)
+                        {
+                            if (o2Numbers[j][i] != '1')
+                            {
+                                o2Numbers.RemoveAt(j);
+                            }
+                            else j++;
+                        }
+                    }
+                    else // zero is most common
+                    {
+                        int j = 0;
+                        while (j < o2Numbers.Count) // remove numbers not matching bit criteria (0)
+                        {
+                            if (o2Numbers[j][i] != '0')
+                            {
+                                o2Numbers.RemoveAt(j);
+                            }
+                            else j++;
+                        }
+                    }
+                }
+
+                // CO2 Rating
+                List<int> co2Ones = CalculateOnes(co2Numbers);
+                if (co2Ones != null)
+                {
+                    if (co2Ones[i] > (co2Numbers.Count - co2Ones[i])) // one is most common, zero is least common
+                    {
+                        int j = 0;
+                        while (j < co2Numbers.Count) // remove numbers not matching bit criteria (0)
+                        {
+                            if (co2Numbers[j][i] != '0')
+                            {
+                                co2Numbers.RemoveAt(j);
+                            }
+                            else j++;
+                        }
+                    }
+                    else if (co2Ones[i] == (co2Numbers.Count - co2Ones[i]))
+                    {
+                        int j = 0;
+                        while (j < co2Numbers.Count) // remove numbers not matching bit criteria (0)
+                        {
+                            if (co2Numbers[j][i] != '0')
+                            {
+                                co2Numbers.RemoveAt(j);
+                            }
+                            else j++;
+                        }
+                    }
+                    else // zero is most common
+                    {
+                        int j = 0;
+                        while (j < co2Numbers.Count) // remove numbers not matching bit criteria (0)
+                        {
+                            if (co2Numbers[j][i] != '1')
+                            {
+                                co2Numbers.RemoveAt(j);
+                            }
+                            else j++;
+                        }
+                    }
+                }
+                
+
+                if (o2Numbers.Count == 1 && co2Numbers.Count == 1) break;
+            }
+
+            double o2Rating = Convert.ToInt32(o2Numbers[0], 2);
+            double co2Rating = Convert.ToInt32(co2Numbers[0], 2);
+            return o2Rating * co2Rating;
+        }
+
+        static List<int> CalculateOnes(List<string> binaryNumbers)
+        {
+            if (binaryNumbers.Count > 1)
+            {
+                List<int> ones = new List<int>();
+                for (int i = 0; i < binaryNumbers[0].Length; i++) ones.Add(0);
+
+                foreach (string binaryNumber in binaryNumbers)
+                {
+                    for (int i = 0; i < binaryNumber.Length; i++)
+                    {
+                        if (binaryNumber[i] == '1') ones[i]++;
+
+                    }
+                }
+                return ones;
+            }
+            return null;
+        }
+    }
+}
---
+++ Day4/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace Day4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = File.ReadAllText("C:/Users/lerich/OneDrive - Microsoft/source/advent-of-code-2021/Day4/input.txt");
+            List<string> inputList = input.Split(new string[] { "\n\n" },
+                               StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            List<int> calledNumbers = inputList[0].Split(',').Select(int.Parse).ToList();
+            List<BingoCard> cards = new List<BingoCard>();
+
+            for (int i = 1; i < inputList.Count; i++)
+            {
+                List<string> numbers = inputList[i].Split(new char[] { ' ', '\n' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+                cards.Add(new BingoCard(numbers));
+            }
+
+            Console.WriteLine("Part 1: " + Part1(calledNumbers, cards));
+            Console.WriteLine("Part 2: " + Part2(calledNumbers, cards));
+        }
+
+        static int Part1(List<int> calledNumbers, List<BingoCard> cards)
+        {
+            for (int number = 0; number < calledNumbers.Count; number++)
+            {
+                foreach (BingoCard card in cards)
+                {
+                    card.MarkCard(calledNumbers[number]);
+                    if (number >= 4) 
+                    {
+                        if (card.CheckForBingo() == true) return card.GetFinalScore(calledNumbers[number]);
+                    } 
+                }
+            }
+            return -1;
+        }
+
+        static int Part2(List<int> calledNumbers, List<BingoCard> cards)
+        {
+            HashSet<BingoCard> winningSet = new HashSet<BingoCard>();
+            BingoCard lastWinner = null;
+            int lastNumber = 0;
+
+            foreach (int calledNumber in calledNumbers)
+            {
+                foreach (BingoCard card in cards)
+                {
+                    if (!winningSet.Contains(card)) card.MarkCard(calledNumber);
+
+                    if (card.CheckForBingo() == true) 
+                    {
+                        if (!winningSet.Contains(card))
+                        {
+                            winningSet.Add(card);
+                            lastWinner = card;
+                            lastNumber = calledNumber;
+                        }       
+                    }
+                }
+            }
+            return lastWinner.GetFinalScore(lastNumber); ;
+        }
+    }
+
+    public class BingoCard
+    {
+        public int[,] card;
+        public bool[,] isMarked;
+
+        public BingoCard(List<string> numbers)
+        {
+            card = new int[5,5];
+            isMarked = new bool[5,5];
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                card[i / 5, i % 5] = int.Parse(numbers[i]);
+                isMarked[i / 5, i % 5] = false;
+            }         
+        }
+        
+        public void MarkCard(int number)
+        {
+            for (int row = 0; row < 5; row++)
+            {
+                for (int col = 0; col < 5; col++)
+                {
+                    if (card[row, col] == number)
+                    {
+                        isMarked[row, col] = true;
+                    }
+                }
+            }
+        }
+
+        public bool CheckForBingo()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                if (isMarked[i, 0] && isMarked[i, 1] && isMarked[i, 2] && isMarked[i, 3] && isMarked[i, 4])
+                    return true;
+                else if (isMarked[0, i] && isMarked[1, i] && isMarked[2, i] && isMarked[3, i] && isMarked[4, i])
+                    return true;               
+            }
+            return false;
+        }
+
+        public int GetFinalScore(int lastNumber)
+        {
+            int score = 0;
+            for (int row = 0; row < 5; row++)
+            {
+                for (int col = 0; col < 5; col++)
+                {
+                    if (!isMarked[row, col]) score += card[row, col];
+                }
+            }
+            return score * lastNumber;
+        }
+    }
+}
